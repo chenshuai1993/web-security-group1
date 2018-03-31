39b85bdf10b63cb02e31b0f800741ba5c7a155d2
shuaishuai.chen <852438363@qq.com>
2018-03-31 16:05:11 +0800

    db

---
+++ db.class.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbms='mysql';     //数据库类型
+$host='123.56.15.74'; //数据库主机名
+$dbName='demo';    //使用的数据库
+$user='root';      //数据库连接用户名
+$pass='123123.';          //对应的密码
+$dsn="$dbms:host=$host;dbname=$dbName";
+
+try {
+    $db = new PDO($dsn, $user, $pass,array(PDO::ATTR_PERSISTENT => true)); //初始化一个PDO对象
+} catch (PDOException $e) {
+    die ("Error!: " . $e->getMessage() . "<br/>");
+}
+
+
+
+
+
