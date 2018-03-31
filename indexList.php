5535d229e5bd50a0da1edf92dfbc05bda0afecd7
gitfav <wangrjmailbox@126.com>
2018-03-31 17:06:04 +0800

    list提交

--- indexList.php
+++ indexList.php
@@ -6,14 +6,18 @@
  * Time: 16:02
  */
 
-require ('./db.class.php');
+if (!empty($_SESSION['user_id'])) {
+    $userInfo['user_id'] = $_SESSION['user_id'];
+    $userInfo['user_name'] = $_SESSION['user_name'];
+}
 
-$sql = 'select * from user';
+require('./db.class.php');
 
-
+$sql = 'select id,username,bbs,fs from user limit 0,10';
 $sth = $db->prepare($sql);
 //$db->bindParam(':calories', $calories, PDO::PARAM_INT);
 //$db->bindParam(':colour', $colour, PDO::PARAM_STR, 12);
 $sth->execute();
 $result = $sth->fetchAll();
-var_dump($result);
+
+require('./html/index.php');
